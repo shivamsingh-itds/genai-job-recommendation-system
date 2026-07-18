ec2581092b45a31c31dc439f192e73464e27ed38
shivamsingh-itds <shivam.itds@gmail.com>
2026-07-18 23:51:16 +0530

    helper functions

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,7 @@
 import fitz # PyMuPDF
 import os 
 from dotenv import load_dotenv
-from openai import OpenAI
+from groq import groq
 
 
 load_dotenv()
@@ -9,7 +9,7 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = groq(api_key=OPENAI_API_KEY)
 
 def extract_text_from_pdf(uploaded_file):
     """
